d32288215f7cd5dba8470465713e6bbd420155ac
lvyahui8 <lvyahui8@126.com>
2018-04-11 21:49:36 +0800

    update

---
+++ src/main/java/org/lyh/netty/example/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.lyh.netty.example;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author lvyahui (lvyahui8@gmail.com,lvyahui8@126.com)
+ * @since 2018/4/2 21:38
+ */
+public class GlobalExceptionHandler extends ChannelHandlerAdapter {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.debug("Local address: {},remote address: {}",ctx.channel().localAddress(),ctx.channel().remoteAddress());
+        logger.error("Exception :",cause);
+        ctx.close();
+    }
+}
