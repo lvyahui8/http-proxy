72754eddc339c4e75d000966b8cf33177c727bda
lvyahui8 <lvyahui8@126.com>
2018-04-28 21:44:48 +0800

    更新启动脚本

--- proxy-server/bin/start.sh
+++ proxy-server/bin/start.sh
@@ -4,11 +4,13 @@ current_path=$(dirname $(which $0))
 
 cd "$current_path/.."
 
+mainClass=org.lyh.http.proxy.HttpProxyServer
+
 nohup java -Dfile.encoding=utf8 -Djava.ext.dirs=./lib -classpath ./conf  \
     -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
-    org.lyh.http.proxy.HttpProxyServer > /dev/null 2>&1 &
+    ${mainClass} > /dev/null 2>&1 &
 
-processId=$(ps axu | grep org.lyh.http.proxy.HttpProxyServer | grep -v grep | awk '{print $2}' )
+processId=$(ps axu | grep ${mainClass} | grep -v grep | awk '{print $2}' )
 
 if [ "$processId" != "" ]; then
     echo "started proxy server [$processId]"
