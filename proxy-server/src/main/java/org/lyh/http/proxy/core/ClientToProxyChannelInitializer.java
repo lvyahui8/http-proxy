4c00fff8362ff4ff75b8da55b0d2b88345566e82
lvyahui8 <lvyahui8@126.com>
2018-05-03 23:43:14 +0800

    update

--- proxy-server/src/main/java/org/lyh/http/proxy/core/ClientToProxyChannelInitializer.java
+++ proxy-server/src/main/java/org/lyh/http/proxy/core/ClientToProxyChannelInitializer.java
@@ -3,7 +3,8 @@ package org.lyh.http.proxy.core;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import org.lyh.http.proxy.filter.AttackRequestFilter;
 
 
@@ -15,11 +16,14 @@ public class ClientToProxyChannelInitializer extends ChannelInitializer<SocketCh
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
-        ch.pipeline().addLast("http-codec",new HttpServerCodec());
-        ch.pipeline().addLast("http-aggregator",new HttpObjectAggregator(1024 * 1024));
+        //ch.pipeline().addLast("http-codec",new HttpServerCodec());
+        ch.pipeline().addLast("http-decoder",new HttpRequestDecoder());
+        ch.pipeline().addLast("file-chunk",new FileChunkAggregator());
+        ch.pipeline().addLast("http-aggregator",new HttpObjectAggregator(1024 * 1024 * 1024));
         ClientToProxyHandler clientToProxyHandler = new ClientToProxyHandler();
         clientToProxyHandler.addFilter(new AttackRequestFilter());
         ch.pipeline().addLast("clientToProxyHandler",clientToProxyHandler);
         ch.pipeline().addLast("globalExceptionHandler",new GlobalExceptionHandler(ch));
+        ch.pipeline().addLast("http-encoder",new HttpResponseEncoder());
     }
 }
